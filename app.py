a7f0eecd0a94e983ba9a8254ecbd116ea95db7c4
Kiran Vuksanaj <kiranv@mit.edu>
2023-12-12 01:21:51 -0500

    module for p2p using peerJS

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
+
+current_peers = []
 
 @app.route("/")
 def homepage():
     return render_template("main.html")
+
+@app.route("/api/post_peer_id",methods=['POST'])
+def post_peer_id():
+    assert request.method == 'POST' and 'pid' in request.form
+    new_pid = request.form['pid']
+    # TODO: on update of current_peers, probably pruning should happen
+    current_peers.append( tuple((new_pid,datetime.now())) )
+    return {'success':True}
+
+@app.route("/api/get_group")
+def get_group():
+    # TODO: prune current peers?
+    return {
+        'success':True,
+        'peer_ids':[ peer[0] for peer in current_peers ]
+        }
 
 if __name__ == "__main__":
     print("in your browser, open the url localhost:5000")
--- static/main.js
+++ static/main.js
@@ -1,1 +1,117 @@
 console.log('hi')
+
+
+// **** PeerJS CONNECTIONS ****
+
+var peer = new Peer();
+var PEER_ID = -1;
+const TIMEOUT_MS = 10000;
+
+
+peer.on('open', function(id) {
+    console.log('My peer ID is: '+id);
+    PEER_ID = id;
+    console.log('(using the default peerJS PeerServer to broker connections.');
+    console.log('post peer id to GA server; ');
+    $.post('/api/post_peer_id',{'pid':id}, function(data,status){
+	console.log('status: '+status);
+    });
+    
+});
+
+peer.on('connection',function(conn) {
+    // received request from peer, either 1. query local cache or 2. query google
+    conn.on('open',function() {
+	conn.on('data',function(data) {
+	    // console.log(data)
+	    // here, there should be distinction made about mode of query! request from google? or check cache?
+	    conn.send( {
+		'type':'response',
+		'success':false,
+		'data':'none'
+	    });
+	});
+    });
+});
+
+
+function doRequest(request,pid) {
+    return new Promise((resolve,reject) => {
+	console.log('attempting connect');
+	const conn = peer.connect(pid);
+	console.log(conn);
+	console.log(request);
+	conn.on('open',function() {
+	    conn.send(request);
+	    conn.on('data', function(data) {
+		// received response from peer, either with query data or a message indicating it not found
+		console.log('received response');
+		// console.log(data);
+		resolve(data);
+	    });
+	});
+    });
+}
+
+async function queryPeers() {
+    const request = {
+	'type':'request',
+	'latlng':(4,4),
+	'radius':30,
+	'query':'restaurants'
+    };
+	    
+    $.get('/api/get_group', function(data) {
+	const peer_ids = data['peer_ids'];
+	if (PEER_ID in peer_ids) {
+	    // remove self from group list
+	    peer_ids.splice( peer_ids.indexOf(PEER_ID), 1 );
+	}
+	const request_local = {...request,
+			       'mode':'local'};
+	console.log(peer_ids);
+
+	const timeoutPromise = new Promise((resolve,reject) => {
+	    // timeout promise
+	    setTimeout(resolve,TIMEOUT_MS,[{'success':false}]);
+	});
+
+	
+	const local_cache_promises = [];
+	for(let i=0; i<peer_ids.length; i++) {
+	    // for each peer, connect and request a local query
+	    local_cache_promises.push( doRequest(request_local,peer_ids[i]) );
+	    
+	}
+	console.log(local_cache_promises);
+	Promise.any([timeoutPromise,Promise.any(local_cache_promises)]).then((data)=>{
+	    console.log('all complete');
+	    console.log(data);
+
+	    // if query not matched, continue to request from google
+	    let dataFound = false;
+	    let foundData = [];
+	    for (response in data){
+		if(response['success']){
+		    dataFound = true;
+		    foundData.append(response['data']);
+		}
+	    }
+	    if (!dataFound) {
+		// request from google!
+		// pick random peer to handle google request
+		google_requester = peer_ids[ Math.floor(Math.random()*peer_ids.length) ];
+		google_request = {...request,
+				  'type':'google'};
+		doRequest(google_request,google_requester).then((data)=>{
+		    return data;
+		});
+	    } else {
+		// return found data!
+		return foundData[0];
+	    }
+	});
+    },'json')
+}
+
+// end peerJS connection code
